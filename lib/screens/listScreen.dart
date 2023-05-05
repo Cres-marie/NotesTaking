552d275e18aaabc86c1046c77e4c593f7629cdb3
GeraldK5 <98259491+GeraldK5@users.noreply.github.com>
2023-05-05 09:05:36 -0700

    Update listScreen.dart

--- lib/screens/listScreen.dart
+++ lib/screens/listScreen.dart
@@ -10,12 +10,36 @@ class ListScreen extends StatefulWidget {
 }
 
 class _ListScreenState extends State<ListScreen> {
+  final search = TextEditingController();
+  final mysql = sqlite();
+  List<Map<String,dynamic>> events=[];
+  List<Map<String, dynamic>> filtered_list =[];
+  Future<void> getAllevents() async{
+    List<Map<String, dynamic>> mynotes = await mysql.getevents();
+    setState((){
+      events = mynotes;
+      filtered_list = mynotes;
+    });
+  }
+  void Searchevent(String query){
+    final suggestions = events.where((note){
+      final title = note['Title'].toString().toLowerCase();
+      final input = query.toLowerCase();
+      return title.contains(input);
+    }).toList();
+    setState(() => filtered_list = suggestions);
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getAllevents();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
 
-        body: SingleChildScrollView(
-        child: Container(
+        body: Container(
           margin: bmargintop,
           padding: bpadding,
           child: Column(
@@ -26,26 +50,81 @@ class _ListScreenState extends State<ListScreen> {
               //Text('Notes', style: bheadings),
               InkWell(
                   onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => BottomBar(),));
+                   Navigator.pop(context);
+                   // Navigator.push(context, MaterialPageRoute(builder: (context) => Navigator.pop(context),));
                   },
                   child: Image.asset('images/backicon.png'),
                 ),
 
               SizedBox(height: 24,),
 
-              InkWell(
-                onTap: () {},
-                child: Image.asset('images/search.png'),
-              ),
+              TextField(
+                    controller: search,
+                    onChanged: (value){
+                      Searchevent(value);
+                    },
+                    
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search, color: Colors.green,),
+                      hintText: 'Search for note',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),    
+                      )
+                    ),
+                  ),
 
               SizedBox(height: 32,),
 
-              Text('To Do', style: bheadings,)
+              Text('To Do', style: bheadings,),
+
+              Expanded(
+                    child: ListView.builder(
+                      itemCount: filtered_list.length,
+                       scrollDirection: Axis.vertical,
+                      itemBuilder: (context, index){
+                        return GestureDetector(
+                       onTap: () {
+                        // Navigator.push(context, MaterialPageRoute(builder: (context) => mynote(note: filtered_list[index]),));
+                       },
+                       onLongPress: (){
+                          // auth.deletenotemessage(context,filtered_list[index]['_id']);
+                          // getAllNotes();
+                       },
+                       child: Container(
+                         height: 120,
+                         width: 150,
+                         margin: EdgeInsets.symmetric( horizontal: 2, vertical: 5),
+                         padding: EdgeInsets.all(5),
+                         decoration: BoxDecoration(
+                           color: Colors.primaries[index % Colors.primaries.length].shade400,
+                           borderRadius: BorderRadius.circular(10),
+                           boxShadow: [BoxShadow(
+                            color: Color.fromARGB(255, 223, 211, 211),
+                            offset: Offset(0.0, 4.0),
+                            blurRadius: 6.0,
+                            ),
+                             ],
+                         ),
+                         child: Column(
+                           crossAxisAlignment: CrossAxisAlignment.start,
+                           children: [
+                             ListTile(
+                               splashColor: Colors.black,
+                               style: ListTileStyle.drawer,
+                               title: Center(child: Text('TITLE: '+filtered_list[index]['Title'],style: TextStyle(fontWeight: FontWeight.bold))),
+                             ),
+                             Expanded(child: Text(filtered_list[index]['starttime'],maxLines: 2, overflow: TextOverflow.ellipsis,style: TextStyle(color: Colors.white),)),
+                               // Text(filtered_list[index]['time'],),
+                           ],
+                         ),
+                       ),
+                          );
+                    })
+                  )
 
             ],
           )
-        ),
-      )
+        )
           
 
 
