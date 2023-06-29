7ca7af867a10c616c6dc9e68b4a1bb4c61bd3ebb
GeraldK5 <98259491+GeraldK5@users.noreply.github.com>
2023-06-29 11:44:49 -0700

    Update todoScreen.dart

--- lib/screens/todoScreen.dart
+++ lib/screens/todoScreen.dart
@@ -25,8 +25,6 @@ class Todo extends StatefulWidget {
 }
 
 class _TodoState extends State<Todo> {
-
-  //bool isChecked = false;
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
   final mysql = sqlite();
   final popupmessage = authenticate();
@@ -44,24 +42,20 @@ Map<DateTime, List<Event>> selectedEvents={};
   if(events.isNotEmpty){
      for(int i=0; i< events.length; i++){
       //print(events[i]['eventtime']);
+    int id = events[i]['_id'];
     String title = events[i]['Title'];
     DateTime date= DateTime.parse(events[i]['eventtime'].toString());
     String eventtime = events[i]['starttime'];
-    print(date);
-
     
          if(selectedEvents[date] != null){
       setState(() {
-         selectedEvents[date]!.add(Event(title: title, time: eventtime,));
-      print('xxxxxxxxxxxxxxxxxxxx');
+         selectedEvents[date]!.add(Event(title: title,id: id, time: eventtime));
         print(selectedEvents);
       });
-     
     }
     else{
       setState(() {
-        selectedEvents[date] = [Event(title: title, time: eventtime)];
-      print('xxxxxxxxxxxxxxxxxxxx');
+        selectedEvents[date] = [Event(title: title,id: id, time: eventtime)];
       print(selectedEvents);
       });
       
@@ -72,7 +66,7 @@ Map<DateTime, List<Event>> selectedEvents={};
   }
  
   }
-  List<Event>_getEventsfromDay(DateTime date){
+  List _getEventsfromDay(DateTime date){
     
     return selectedEvents[date] ?? [];
   }
@@ -105,9 +99,8 @@ Map<DateTime, List<Event>> selectedEvents={};
   @override
   void initState() {
     super.initState();
-    getdata(); 
- 
-    //SmolleyNotification.initialize(fNp);
+    getdata();
+    SmolleyNotification.checkScheduledNotifications(flutterLocalNotificationsPlugin);
   }
   @override
   void dispose() {
@@ -118,7 +111,7 @@ Map<DateTime, List<Event>> selectedEvents={};
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
+extendBodyBehindAppBar: true,
       appBar: AppBar(
          
       //   //title:Text('To Do', style: bheadings,),
@@ -298,90 +291,95 @@ Map<DateTime, List<Event>> selectedEvents={};
             
                 ..._getEventsfromDay(_focusedDay).map((event) => SingleChildScrollView(
               
-                  child: Container(
-                    padding: EdgeInsets.only(right: 20, left: 20, top: 10),
-                  
-                    child: AnimatedOpacity(
-                      opacity: event.isChecked ? 0.5 : 1.0,
-                      duration: Duration(seconds: 2),
-                      child: Container(
-                         height: 120,
-                         width: 550,
-                         margin: EdgeInsets.symmetric( horizontal: 2, vertical: 5),
-                         
-                         padding: bpadding,
-                         //padding: EdgeInsets.all(5),
-                         decoration: BoxDecoration(
-                            color:  Color.fromARGB(255, 230, 190, 165),
-                           //color: Colors.primaries[Random().nextInt(Colors.primaries.length)].withOpacity(0.6),
-                           borderRadius: BorderRadius.circular(10),
-                           boxShadow: [BoxShadow(
-                            color: Color.fromARGB(255, 223, 211, 211),
-                            offset: Offset(0.0, 4.0),
-                            blurRadius: 6.0,
-                            ),
-                             ],
-                         ),
-                         child: Column(
-                           crossAxisAlignment: CrossAxisAlignment.start,
-                           children: [
-                            
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: AnimatedOpacity(
-                                    opacity: event.isChecked ? 0.5 : 1.0, // Adjust the opacity based on the isChecked value,
-                                    duration: Duration(seconds: 2),// Set the duration of the fade animation
-                                    child: Text(
-                                      '${event.title[0].toUpperCase()}${event.title.substring(1)}',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 20,
-                                        decoration: event.isChecked
-                                          ? TextDecoration.lineThrough // Apply strikethrough style when isChecked is true
-                                          : TextDecoration.none, // Remove decoration when isChecked is false
+                  child: GestureDetector(
+                    onLongPress: () {
+                      popupmessage.deletenotemessage(context,event.id, false);
+                    },
+                    child: Container(
+                      padding: EdgeInsets.only(right: 20, left: 20, top: 10),
+                    
+                      child: AnimatedOpacity(
+                        opacity: event.isChecked ? 0.5 : 1.0,
+                        duration: Duration(seconds: 2),
+                        child: Container(
+                           height: 120,
+                           width: 550,
+                           margin: EdgeInsets.symmetric( horizontal: 2, vertical: 5),
+                           
+                           padding: bpadding,
+                           //padding: EdgeInsets.all(5),
+                           decoration: BoxDecoration(
+                              color:  Color.fromARGB(255, 230, 190, 165),
+                             //color: Colors.primaries[Random().nextInt(Colors.primaries.length)].withOpacity(0.6),
+                             borderRadius: BorderRadius.circular(10),
+                             boxShadow: [BoxShadow(
+                              color: Color.fromARGB(255, 223, 211, 211),
+                              offset: Offset(0.0, 4.0),
+                              blurRadius: 6.0,
+                              ),
+                               ],
+                           ),
+                           child: Column(
+                             crossAxisAlignment: CrossAxisAlignment.start,
+                             children: [
                               
-                                      ) 
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: AnimatedOpacity(
+                                      opacity: event.isChecked ? 0.5 : 1.0, // Adjust the opacity based on the isChecked value,
+                                      duration: Duration(seconds: 2),// Set the duration of the fade animation
+                                      child: Text(
+                                        '${event.title[0].toUpperCase()}${event.title.substring(1)}',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20,
+                                          decoration: event.isChecked
+                                            ? TextDecoration.lineThrough // Apply strikethrough style when isChecked is true
+                                            : TextDecoration.none, // Remove decoration when isChecked is false
+                                
+                                        ) 
+                                      ),
                                     ),
                                   ),
-                                ),
-                    
-                                Checkbox(
-                                  value: event. isChecked ?? false, 
-                                  onChanged: (value){
-                                      setState(() {
-                                        event.isChecked = value ?? false;
-
-                                        if (event.isChecked) {
-
-                                          //deleterecord(event.id);
                       
+                                  Checkbox(
+                                    value: event. isChecked ?? false, 
+                                    onChanged: (value){
                                         setState(() {
+                                          event.isChecked = value ?? false;
+                  
+                                          if (event.isChecked) {
+                  
+                                            //deleterecord(event.id);
                         
-                                    });
-                                  }
+                                          setState(() {
+                          
                                       });
-                                  }
-                                )
-                              ],
-                            ),
-                                      
-                            Text(event.time, style: TextStyle(fontSize: 12,) ),
-                                      
-                                      
-                                      
-                            
-                            // ListTile(
-                            //    splashColor: Colors.black,
-                            //    style: ListTileStyle.drawer,
-                            //    //title: Center(child: Text(event.title,style: TextStyle(fontWeight: FontWeight.bold))),
-                                      
-                            //  ),
-                            //  Expanded(child: Text(event.time,maxLines: 2, overflow: TextOverflow.ellipsis,style: TextStyle(color: Colors.white),)),
-                                
-                           ],
+                                    }
+                                        });
+                                    }
+                                  )
+                                ],
+                              ),
+                                        
+                              Text(event.time, style: TextStyle(fontSize: 12,) ),
+                                        
+                                        
+                                        
+                              
+                              // ListTile(
+                              //    splashColor: Colors.black,
+                              //    style: ListTileStyle.drawer,
+                              //    //title: Center(child: Text(event.title,style: TextStyle(fontWeight: FontWeight.bold))),
+                                        
+                              //  ),
+                              //  Expanded(child: Text(event.time,maxLines: 2, overflow: TextOverflow.ellipsis,style: TextStyle(color: Colors.white),)),
+                                  
+                             ],
+                           ),
                          ),
-                       ),
+                      ),
                     ),
                   ),
                 ),)
@@ -391,15 +389,14 @@ Map<DateTime, List<Event>> selectedEvents={};
             ),
           ),
       ),
-      
-    
+
       floatingActionButton: FloatingActionButton(
         onPressed: (){
         showDialog(context: context, builder: (context){
           DateTime time = DateTime.now();
           final dDay = DateTime.utc(DateTime.now().year, DateTime.now().month, DateTime.now().day);
           DateTime date = dDay.toUtc();
-          bool remainder = false;
+          
           print(date);
          return StatefulBuilder(
            builder: (BuildContext context, void Function(void Function()) setState) { 
@@ -409,7 +406,7 @@ Map<DateTime, List<Event>> selectedEvents={};
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text('Enter Your Event'),
-                //Icon(Icons.calendar_month, color:  Color(0xFF52B848),)
+                Icon(Icons.calendar_month, color:  Color(0xFF52B848),)
               ],
             ),
              content: SingleChildScrollView(
@@ -426,68 +423,43 @@ Map<DateTime, List<Event>> selectedEvents={};
                              }
                              return null;
                            },
+                           style: TextStyle(fontSize: 20),
                            controller: title,
                            decoration: InputDecoration(
                              hintText: 'Remind me to...',
-                             suffixIcon: Icon(Icons.create_sharp, color: bselected,),
-                             border: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                                color: biconcolor
-                              )
-                             )
+                             suffixIcon: Icon(Icons.create_sharp),
+                             border: UnderlineInputBorder()
                            ),
                          ),
                         SizedBox(height: 20,),
-                         Text('Select Time', style: TextStyle( fontSize: 20),),
+                         Text('SELECT TIME', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
                          SizedBox(height: 20,),
                          Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            // Expanded(
-                            //   child: InkWell(
-                            //     onTap: () async{
-                            //         final mydate = await pickdatetime(time, true);
-                            //         if(mydate == null) return null;
-                            //         final dDay = DateTime.utc(mydate.year, mydate.month, mydate.day).toUtc();
-                            //         print(dDay);
-                            //         setState((){
-                            //           date = dDay;
-                            //         } );
-                            //     },
-                            //     child: Text('Date: '+DateFormat.yMMMd().format(date))),
-                            // ),
+                            Expanded(
+                              child: InkWell(
+                                onTap: () async{
+                                    final mydate = await pickdatetime(time, true);
+                                    if(mydate == null) return null;
+                                    final dDay = DateTime.utc(mydate.year, mydate.month, mydate.day).toUtc();
+                                    print(dDay);
+                                    setState((){
+                                      date = dDay;
+                                    } );
+                                },
+                                child: Text('Date: '+DateFormat.yMMMd().format(date))),
+                            ),
                             InkWell(
                               onTap: () async{
                                  final mydate = await pickdatetime(time, false);
                                  if(mydate == null) return null;
                                  setState(()=> time = DateTime(date.year, date.month, date.day, mydate.hour, mydate.minute));
                               },
-                              child:  Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Icon(Icons.schedule),
-                                  SizedBox(width: 15),
-                                  Text(DateFormat('h:mm a').format(time)),
-                                ],
-                              ),
-                              //Text('Time: '+DateFormat.Hm().format(time))
-                              
-                            ),
+                              child: Text('Time: '+DateFormat.Hm().format(time))),
                           ],
                          ),
-                          SizedBox(height: 20,),
-                         Divider(color: Colors.grey,),
                          
-                           SwitchListTile(
-                            title: Text('Remind me daily'),
-                            activeColor: bselected,
-                            value: remainder, 
-                            onChanged: (value){
-                                setState(
-                                  () =>remainder = value,
-                                );
-                            }),
 
                  ],
                )),
@@ -499,22 +471,23 @@ Map<DateTime, List<Event>> selectedEvents={};
                           mysql.insertevent(title.text,date,time);
                           //Navigator.push(context, MaterialPageRoute(builder: (context)=> ListScreen())                 
                           print(selectedEvents.entries);
+                          
                           SmolleyNotification.ShowNotification(title: 'Hey there! Do you know what time it is?', body: title.text, fn: flutterLocalNotificationsPlugin, time: time);
-                          //  SmolleyNotification().dailyNotification(time, 'Hey there! Do you know what time it is?', title.text);
-                          //  SmolleyNotification().dailyNotification(time.subtract(Duration(minutes: 30)), 'Hey there! Its almost Time', title.text);
-                           Navigator.of(context).pop();
-                           getdata();
+
+                          
+                          SmolleyNotification.checkScheduledNotifications(flutterLocalNotificationsPlugin);
+                          
+                          getdata();
+                          Navigator.of(context).pop();
+                           
                          }
                           }, 
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(biconcolor),
-                          ),
                           child: Text("Add Event")),
                            TextButton(
                      onPressed: (){
                          Navigator.of(context).pop();
-            }, 
-            child: Text("Cancel", style: TextStyle(color: biconcolor),))
+            }, child: Text("Cancel")),
+           
              ],
            );
             },
